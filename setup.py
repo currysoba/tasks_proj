9b25d9083b5abcea89e46d84c8865440449e26e4
currysoba <21022654+currysoba@users.noreply.github.com>
2020-09-13 22:48:53 +0900

    最新版のtinydbだとpytestがエラーになるのでインストールするtinydbを3.15.1にした

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
 
-    install_requires=['click', 'tinydb', 'six', 'pytest', 'pytest-mock'],
+    install_requires=['click', 'tinydb==3.15.1', 'six', 'pytest', 'pytest-mock'],
+#    install_requires=['click', 'tinydb', 'six', 'pytest', 'pytest-mock'],
     tests_require=['pytest', 'pytest-mock'],
     extras_require={'mongo': 'pymongo'},
 
